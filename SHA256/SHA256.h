ec822ef6581764696e0cfb82dfd328301b345750
Sjors Provoost <sjors@sprovoost.nl>
2014-08-02 23:10:54 +0200

    Support iOs and Mac.

---
+++ SHA256/SHA256.h
@@ -0,0 +1,18 @@
+//
+//  SHA256.h
+//  SHA256
+//
+//  Created by Sjors Provoost on 02-08-14.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHA256.
+FOUNDATION_EXPORT double SHA256VersionNumber;
+
+//! Project version string for SHA256.
+FOUNDATION_EXPORT const unsigned char SHA256VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHA256/PublicHeader.h>
+
+
